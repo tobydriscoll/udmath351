4e76ae537c54f7b0d62144b7af1fc9914bd28e68
Toby Driscoll <driscoll@udel.edu>
2021-10-05 14:28:54 -0400

    Read flashcards from CSV

---
+++ flashcards/update.jl
@@ -0,0 +1,15 @@
+using CSV,DataFrames,JSON
+
+df = CSV.File("flashcards.csv") |> DataFrame
+# n = size(df,1)
+# out = Array{Dict{String,String}}(undef,n)
+# for i in 1:n 
+#     out[i]["front"] = df.Front[i]
+#     out[i]["back"] = df.Back[i]
+# end
+out = [ r[[:Front,:Back]] for r in eachrow(df) ]
+open("flashcards.json","w") do io 
+    JSON.print(io,out)
+end
+
+CSV.write("credits.csv",combine(groupby(df,:Name),nrow))
